649b60729ee52a333c0401d9947c3c1df50e32da
Kleyson Castro <kleysoncastro99@gmail.com>
2018-09-09 23:22:01 -0400

    Teste para abri tela com evento de click

--- src/br/com/castro/controller/Login.java
+++ src/br/com/castro/controller/Login.java
@@ -1,5 +1,17 @@
 package br.com.castro.controller;
 
+import br.com.castro.application.Main;
+
 public class Login {
+	
+	
+	Main main = new Main();
+	
+	public void testeTela() {
+		
+		main.startPelaPrincipal();
+	}
+	
+	
 
 }
