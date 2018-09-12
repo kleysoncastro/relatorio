334a5380db3663623111b54fb3340cd30d35e6d2
Kleyson Castro <kleysoncastro99@gmail.com>
2018-09-11 20:05:21 -0400

    Teste de funcao par logar

--- src/br/com/castro/controller/Login.java
+++ src/br/com/castro/controller/Login.java
@@ -3,22 +3,40 @@ package br.com.castro.controller;
 import br.com.castro.application.Main;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class Login {
 	
 	@FXML
 	private Button BTLogin;
 	
+	@FXML
+	private TextField idLogin;
+	
+	@FXML
+	private PasswordField idPass;
 	
 	Main main = new Main();
 	
-	public void testeTela() {
+	
+	@FXML
+	private void logar() {
 		
-		main.startPelaPrincipal();
-		BTLogin.getScene().getWindow().hide();// fecha a cena autal
-
+		if(idPass.getText().equals("12345")) {
+			
+			main.startPelaPrincipal();
+			BTLogin.getScene().getWindow().hide();// fecha a cena autal
+			
+			
+		} else {
+			
+			System.out.println("erro");
+		}
+		
 	}
 	
+		
 	
 
 }
